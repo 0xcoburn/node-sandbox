c849c3a283f28b1b16c320eb6e91c753876f3e06
Matthew Coburn <mattcoburn24@gmail.com>
2021-01-10 12:10:27 -0800

    added html files to send with my responses, created a util folder with a path file to create a root path

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,16 @@
+//core modules
+const path = require("path");
+
+//3rd party modules
 const express = require("express");
+
+//my file imports
+const rootDir = require("../utils/path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='post'><input name='title' type='text'><button type='submit'>add product></button></form>"
-  );
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
